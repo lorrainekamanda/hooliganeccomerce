5726a2d319788e0f888a16e909fc487c78d4206e
Rain <user@localhost.localdomain>
2020-11-03 01:00:40 -0500

    new changes

--- static_files/js/script.js
+++ static_files/js/script.js
@@ -10,3 +10,42 @@ $(window).scroll(function() {
     $('html, body').animate({ scrollTop: 0 }, "slow");
     return false;
   });
+
+  (function($) {
+    /*------------------
+      Navigation
+    --------------------*/
+    $('.main-menu').slicknav({
+      prependTo:'.main-navbar .container',
+      closedSymbol: '<i class="flaticon-right-arrow"></i>',
+      openedSymbol: '<i class="flaticon-down-arrow"></i>'
+    });
+  
+  
+    /*------------------
+      ScrollBar
+    --------------------*/
+    $(".cart-table-warp, .product-thumbs").niceScroll({
+      cursorborder:"",
+      cursorcolor:"#afafaf",
+      boxzoom:false
+    });
+  
+  
+    /*------------------
+      Category menu
+    --------------------*/
+    $('.category-menu > li').hover( function(e) {
+      $(this).addClass('active');
+      e.preventDefault();
+    });
+    $('.category-menu').mouseleave( function(e) {
+      $('.category-menu li').removeClass('active');
+      e.preventDefault();
+    });
+  
+  
+ 
+  
+  
+  
