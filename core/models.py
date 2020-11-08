d93f960eef5816e992715cd8b4443f457217bf3c
Rain <user@localhost.localdomain>
2020-11-08 17:02:21 -0500

    default

--- core/models.py
+++ core/models.py
@@ -59,7 +59,7 @@ class Profile(models.Model):
 class Item(models.Model):
    title = models.CharField(max_length = 100)
    price = models.FloatField()
-   discount_price = models.FloatField(blank = True,null = True)
+   discount_price = models.FloatField(default = 0,blank = True,null = True)
    category = models.CharField(choices = CATEGORY_CHOICES,max_length = 2)
    label= models.CharField(choices = LABEL_CHOICES,max_length = 2)
    slug = models.SlugField()
